ae6231cef98a75aac38d13ac3a079b88e5946778
queirozzrafael <rafael.queirozvieira@gmail.com>
2024-04-09 14:40:31 -0300

    update files

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function addTask()  {
+    const newTask = document.getElementById('addTask').value
+    createNewTask(newTask)
+
+}
+
+function createNewTask (text) {
+    const listaTarefas = document.getElementById('')
+}
